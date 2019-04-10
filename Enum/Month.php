2777da7c4130520ccde4efc523fc0be837d33cb1
NesCafe62 <U.238@rambler.ru>
2019-04-11 00:33:52 +0300

    Add files via upload

---
+++ Enum/Month.php
@@ -0,0 +1,33 @@
+<?php
+namespace Pandora3\Libs\Time\Enum;
+
+class Month {
+
+	public const None = 0;
+
+	public const January = 1;
+	public const February = 2;
+	public const March = 3;
+	public const April = 4;
+	public const May = 5;
+	public const June = 6;
+	public const July = 7;
+	public const August = 8;
+	public const September = 9;
+	public const October = 10;
+	public const November = 11;
+	public const December = 12;
+
+	/**
+	 * @param int $month
+	 * @param int|null $year
+	 * @return int
+	 */
+	public static function numberOfDays(int $month, $year = null): int {
+		if ($year === null) {
+			$year = (int) date('Y');
+		}
+		return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+	}
+
+}
---
+++ Enum/Week.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pandora3\Libs\Time\Enum;
+
+class Week {
+
+	public const None = 0;
+
+	public const Monday = 1;
+	public const Tuesday = 2;
+	public const Thursday = 3;
+	public const Wednesday = 4;
+	public const Friday = 5;
+	public const Saturday = 6;
+	public const Sunday = 7;
+
+}
