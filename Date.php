71567867424494dd373bc10c6ec0b78a6ea353a8
NesCafe62 <U.238@rambler.ru>
2019-04-10 23:34:19 +0200

    Update Date.php

--- Date.php
+++ Date.php
@@ -1,31 +1,155 @@
 <?php
-namespace pandora3\libs\Time;
+namespace Pandora3\Libs\Time;
 
+use DateInterval;
+use DateTimeZone;
 
-class Date {
+/**
+ * @property-read int $year
+ * @property-read int $month
+ * @property-read int $day
+ * @property-read int $dayOfWeek
+ */
+class Date extends \DateTimeImmutable {
 
-	/**
-	 * @var int $time
-	 */
-	protected $time;
-
-	/**
-	 * @var int $day
-	 */
-	protected $day;
-
-	/**
-	 * @var int $month
-	 */
-	protected $month;
+	public const FormatMysql = 'Y-m-d';
 
 	/**
-	 * @var int $year
+	 * @param string|\DateTimeInterface $time
+	 * @param string|DateTimeZone|null $timezone
 	 */
-	protected $year;
+	public function __construct($time = 'now', $timezone = null) {
+	    if (is_string($timezone)) {
+			$timezone = new DateTimeZone($timezone);
+        }
 
-	public function __construct($time) {
-		$this->time = $time;
+		if (!is_object($time) || !($time instanceof \DateTimeInterface)) {
+			$time = new \DateTime($time);
+		}
+		parent::__construct($time->format(self::FormatMysql), $timezone);
+	}
+
+	/**
+	 * @param string|int $year
+	 * @param string|int $month
+	 * @param string|int $day
+	 * @return static
+	 */
+	public static function create($year, $month, $day): self {
+		return (new static())
+			->setDate((int) $year, (int) $month, (int) $day);
+	}
+
+	/**
+	 * @param string $format
+	 * @param string|null $time
+	 * @param DateTimeZone|null $timezone
+	 * @return static|null
+	 */
+	public static function createFromFormat($format, $time, $timezone = null) {
+		$date = $time ? parent::createFromFormat($format, $time) : null;
+		return $date ? new static($date, $timezone) : null;
+	}
+
+	/**
+	 * @param string $property
+	 * @return mixed
+	 */
+	public function __get(string $property) {
+		$methodName = 'get'.ucfirst($property);
+		if (method_exists($this, $methodName)) {
+			return $this->{$methodName}();
+		}
+		return null;
+	}
+
+	/**
+	 * @return static
+	 */
+	public static function now(): self {
+		return new static('now');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function toMysql(): string {
+		return $this->format(self::FormatMysql);
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getYear(): int {
+		return (int) $this->format('Y');
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getMonth(): int {
+		return (int) $this->format('m');
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getDay(): int {
+		return (int) $this->format('d');
+	}
+
+	/**
+	 * Gets day of week 1 (for Monday) through 7 (for Saturday)
+	 * @return int
+	 */
+	public function getDayOfWeek(): int {
+		return (int) $this->format('N');
+	}
+
+	/**
+	 * @param int $year
+	 * @return bool
+	 */
+	public static function isLeapYear(int $year): bool {
+		$date = \DateTime::createFromFormat('Y', $year);
+		return (int) $date->format('L') === 1;
+	}
+
+	/**
+	 * @return DateTime
+	 */
+	public function toDateTime(): DateTime {
+		return new DateTime($this);
+	}
+
+	/**
+	 * @param string $interval
+	 * @return static
+	 */
+	public function addInterval(string $interval) {
+		$date = $this->add(DateInterval::createFromDateString($interval));
+		return $date->setTime(0, 0);
+	}
+
+	/**
+	 * @param string $interval
+	 * @return bool|static
+	 */
+	public function subInterval(string $interval) {
+		$date = $this->sub(DateInterval::createFromDateString($interval));
+		return $date->setTime(0, 0);
+	}
+
+	/**
+	 * @param nuLL|string|static $date
+	 * @param string $format
+	 * @return string
+	 */
+	public static function convert($date, string $format): string {
+		if (!($date instanceof \DateTimeInterface)) {
+			$date = self::createFromFormat(self::FormatMysql, $date);
+		}
+		return $date ? $date->format($format) : '';
 	}
 
 }
